27284f359f106fd2d47ec84bd18a6e652c6dd33d
BuildTools <unconfigured@null.spigotmc.org>
2023-12-01 21:29:09 +0800

    finsh rewrite extract code to adapt new api.Also finsh plot graph.

--- test2.py
+++ test2.py
@@ -1,26 +1,161 @@
+import requests
+import json
+from datetime import datetime
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Sample data for the table
-data = [
-    ["Name", "Age", "City"],
-    ["John", 30, "New York"],
-    ["Alice", 25, "Los Angeles"],
-    ["Bob", 35, "Chicago"],
-]
+json_file_path = 'response_1700983905482.json'
+with open(json_file_path, 'r', encoding='utf-8') as file:
+    data_json = json.load(file)
+target_location_name = "淡水區"
+locations = data_json['records']['locations']   # Access the 'locations' list
+data = []
+# Filter locations based on the target location name
+danshui_data = [location for location in locations if location["location"][3]["locationName"] == target_location_name]
 
-# Create a figure and axis
-fig, ax = plt.subplots()
+for location in danshui_data:
+    location_data = {'Location': location['location'][3]['locationName'], 'Elements': []}
 
-# Hide the axes
-ax.axis("off")
+    for element in location['location'][3].get('weatherElement', []):
+        element_data = {'Element': element['elementName'], 'TimeEntries': []}
 
-# Create the table
-table = ax.table(cellText=data, loc="center", cellLoc="center", colLabels=None)
+        for time_entry in element.get('time', []):
+            time_entry_data = {
+                'StartTime': time_entry['startTime'],
+                'EndTime': time_entry['endTime'],
+                'Value': f"{time_entry['elementValue'][0]['value']} {time_entry['elementValue'][0]['measures']}"
+            }
 
-# Customize the appearance of the table
-table.auto_set_font_size(False)
-table.set_fontsize(12)
-table.scale(1.2, 1.2)  # Adjust the table size
+            element_data['TimeEntries'].append(time_entry_data)
+
+        location_data['Elements'].append(element_data)
+
+    data.append(location_data)
+
+
+data_np = np.array(data)
+'''
+for location_data in data:
+    print(f"Location: {location_data['Location']}")
+    for element_data in location_data['Elements']:
+        print(f"Element: {element_data['Element']}")
+        for time_entry_data in element_data['TimeEntries']:
+            print(f"Start Time: {time_entry_data['StartTime']}")
+            print(f"End Time: {time_entry_data['EndTime']}")
+            print(f"Value: {time_entry_data['Value']}")
+            print()
+'''
+#print(data_np)
+
+# Initialize an empty list to store precipitation probability values
+precipitation_probability = []
+
+# Iterate through each location in the data
+for location_data in data_np:
+    # Iterate through each element in the location
+    for element_data in location_data['Elements']: 
+        # Check if the element is 'PoP12h' (12-hour precipitation probability)
+        if element_data['Element'] == 'PoP12h':
+            # Iterate through each time entry in the element
+            for time_entry in element_data['TimeEntries']:
+                # Extract the 'Value' and convert it to a numeric value
+                value_str = time_entry['Value'].split()[0]
+                value = float(value_str) if value_str.isdigit() else np.nan
+
+                # Append the value to the list
+                precipitation_probability.append(value)
+
+# Now 'precipitation_probability' contains the 12-hour precipitation probability values
+print(location_data['Elements'])
+print(precipitation_probability)
+
+average_temp = []
+
+# Iterate through each location in the data
+for location_data in data_np:
+    # Iterate through each element in the location
+    for element_data in location_data['Elements']:
+        # Check if the element is 'PoP12h' (12-hour precipitation probability)
+        if element_data['Element'] == 'T':
+            # Iterate through each time entry in the element
+            for time_entry in element_data['TimeEntries']:
+                # Extract the 'Value' and convert it to a numeric value
+                value_str = time_entry['Value'].split()[0]
+                value = float(value_str) if value_str.isdigit() else np.nan
+
+                # Append the value to the list
+                average_temp.append(value)
+
+# Now 'precipitation_probability' contains the 12-hour precipitation probability values
+print(average_temp)
+
+wx_values = []
+
+# Specify the target location and element
+target_element_name = "Wx"
+target_location_name = "瑞芳區"
+# Iterate through each location in your data
+for location_entry in data_json["records"]["locations"]:
+    for weather_element in location_entry["location"][3]["weatherElement"]:
+        if weather_element["elementName"] == target_element_name:
+            # Iterate through each time entry for the specified element
+            for time_entry in weather_element["time"]:
+                value_str = time_entry["elementValue"][0]["value"]
+                wx_values.append(value_str)
+
+print(wx_values)
+
+timestamps = []
+
+# Iterate through each location in your data
+for location_entry in data_json["records"]["locations"]:
+    for weather_element in location_entry["location"][3]["weatherElement"]:
+        if weather_element["elementName"] == 'T':
+            # Iterate through each time entry for the specified element
+            for time_entry in weather_element["time"]:
+                start_time = time_entry["startTime"]
+                end_time = time_entry["endTime"]
+                timestamps.extend([start_time, end_time])
+
+print(timestamps)
+#print precipitation_probability plot
+x_precipitation_probability = timestamps
+y_precipitation_probability = precipitation_probability
+
+formatted_x = [f'{start} ->\n{end}' for start, end in zip(x_precipitation_probability[::2], x_precipitation_probability[1::2])]
+# Create a plot with labels and title
+plt.plot(formatted_x, y_precipitation_probability, label='Precipitation Probability')
+plt.xlabel('Time Range')
+plt.ylabel('Precipitation Probability')
+plt.title('Tamsui Weather Forecast')
+
+# Rotate x-axis labels
+plt.xticks(rotation=45)
+
+# Add grid lines and legend
+plt.grid(True)
+plt.legend()
 
 # Show the plot
+plt.tight_layout()
+plt.show()
+
+#average temp plot
+y_temp = average_temp
+
+# Create a plot with labels and title
+plt.plot(formatted_x, y_temp, label='average temperature')
+plt.xlabel('Time Range')
+plt.ylabel('Temperature')
+plt.title('Tamsui Weather Forecast')
+
+# Rotate x-axis labels
+plt.xticks(rotation=45)
+
+# Add grid lines and legend
+plt.grid(True)
+plt.legend()
+
+# Show the plot
+plt.tight_layout()
 plt.show()
--- weather.py
+++ weather.py
@@ -4,122 +4,66 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
-json_file_path = 'response_1700983905482.json'
+json_file_path = 'weather_data.json'
 with open(json_file_path, 'r', encoding='utf-8') as file:
     data_json = json.load(file)
 target_location_name = "淡水區"
-locations = data_json['records']['locations']   # Access the 'locations' list
-data = []
-# Filter locations based on the target location name
-danshui_data = [location for location in locations if location["location"][3]["locationName"] == target_location_name]
+# Create a dictionary to store location information
+# Create a dictionary to store location information
+data = {}
 
-for location in danshui_data:
-    location_data = {'Location': location['location'][3]['locationName'], 'Elements': []}
+# Iterate through the data to find the target location
+for dataset_entry in data_json:
+    if dataset_entry["locationsName"] == "新北市":
+        for location_entry in dataset_entry["location"]:
+            if location_entry["locationName"] == target_location_name:
+                # Extract information for the desired location
+                data[target_location_name] = {
+                    "geocode": location_entry["geocode"],
+                    "lat": location_entry["lat"],
+                    "lon": location_entry["lon"],
+                    "weatherElements": []
+                }
 
-    for element in location['location'][3].get('weatherElement', []):
-        element_data = {'Element': element['elementName'], 'TimeEntries': []}
+                # Iterate through weather elements
+                for weather_element in location_entry["weatherElement"]:
+                    element_info = {
+                        "elementName": weather_element["elementName"],
+                        "description": weather_element["description"],
+                        "time": []
+                    }
 
-        for time_entry in element.get('time', []):
-            time_entry_data = {
-                'StartTime': time_entry['startTime'],
-                'EndTime': time_entry['endTime'],
-                'Value': f"{time_entry['elementValue'][0]['value']} {time_entry['elementValue'][0]['measures']}"
-            }
+                    # Iterate through time entries for the weather element
+                    for time_entry in weather_element["time"]:
+                        time_info = {
+                            "startTime": time_entry["startTime"],
+                            "endTime": time_entry["endTime"],
+                            "value": time_entry["elementValue"][0]["value"],
+                            "measures": time_entry["elementValue"][0]["measures"]
+                        }
+                        element_info["time"].append(time_info)
 
-            element_data['TimeEntries'].append(time_entry_data)
+                    # Add weather element information to the location
+                    data[target_location_name]["weatherElements"].append(element_info)
+#降雨機率
+pop12h_values = [float(entry['value']) if entry['value'].isdigit() else np.nan for entry in data['淡水區']['weatherElements'][0]['time']]
+#print(f"The PoP12h values are: {pop12h_values}")
+#平均溫度
+average_temp = [float(entry['value']) if entry['value'].isdigit() else np.nan for entry in data['淡水區']['weatherElements'][1]['time']]
+#print(f"The average_temp are: {average_temp}")
+#最大體感
+MaxAT = [float(entry['value']) if entry['value'].isdigit() else np.nan for entry in data['淡水區']['weatherElements'][5]['time']]
+#print(f"The MaxAT values are: {MaxAT}")
+#天氣現象
+Wx = [int(entry['value']) if entry['value'].isdigit() else entry['value'] for entry in data['淡水區']['weatherElements'][6]['time']]
+#print(f"The Wx values are: {Wx}")
+#時間戳
+timestamps = [timestamp for entry in data['淡水區']['weatherElements'][0]['time'] for timestamp in (entry['startTime'], entry['endTime'])]
+#print(timestamps)
 
-        location_data['Elements'].append(element_data)
-
-    data.append(location_data)
-
-
-data_np = np.array(data)
-'''
-for location_data in data:
-    print(f"Location: {location_data['Location']}")
-    for element_data in location_data['Elements']:
-        print(f"Element: {element_data['Element']}")
-        for time_entry_data in element_data['TimeEntries']:
-            print(f"Start Time: {time_entry_data['StartTime']}")
-            print(f"End Time: {time_entry_data['EndTime']}")
-            print(f"Value: {time_entry_data['Value']}")
-            print()
-'''
-#print(data_np)
-
-# Initialize an empty list to store precipitation probability values
-precipitation_probability = []
-
-# Iterate through each location in the data
-for location_data in data_np:
-    # Iterate through each element in the location
-    for element_data in location_data['Elements']: 
-        # Check if the element is 'PoP12h' (12-hour precipitation probability)
-        if element_data['Element'] == 'PoP12h':
-            # Iterate through each time entry in the element
-            for time_entry in element_data['TimeEntries']:
-                # Extract the 'Value' and convert it to a numeric value
-                value_str = time_entry['Value'].split()[0]
-                value = float(value_str) if value_str.isdigit() else np.nan
-
-                # Append the value to the list
-                precipitation_probability.append(value)
-
-# Now 'precipitation_probability' contains the 12-hour precipitation probability values
-print(precipitation_probability)
-
-average_temp = []
-
-# Iterate through each location in the data
-for location_data in data_np:
-    # Iterate through each element in the location
-    for element_data in location_data['Elements']:
-        # Check if the element is 'PoP12h' (12-hour precipitation probability)
-        if element_data['Element'] == 'T':
-            # Iterate through each time entry in the element
-            for time_entry in element_data['TimeEntries']:
-                # Extract the 'Value' and convert it to a numeric value
-                value_str = time_entry['Value'].split()[0]
-                value = float(value_str) if value_str.isdigit() else np.nan
-
-                # Append the value to the list
-                average_temp.append(value)
-
-# Now 'precipitation_probability' contains the 12-hour precipitation probability values
-print(average_temp)
-
-wx_values = []
-
-# Specify the target location and element
-target_element_name = "Wx"
-target_location_name = "瑞芳區"
-# Iterate through each location in your data
-for location_entry in data_json["records"]["locations"]:
-    for weather_element in location_entry["location"][3]["weatherElement"]:
-        if weather_element["elementName"] == target_element_name:
-            # Iterate through each time entry for the specified element
-            for time_entry in weather_element["time"]:
-                value_str = time_entry["elementValue"][0]["value"]
-                wx_values.append(value_str)
-
-print(wx_values)
-
-timestamps = []
-
-# Iterate through each location in your data
-for location_entry in data_json["records"]["locations"]:
-    for weather_element in location_entry["location"][3]["weatherElement"]:
-        if weather_element["elementName"] == 'T':
-            # Iterate through each time entry for the specified element
-            for time_entry in weather_element["time"]:
-                start_time = time_entry["startTime"]
-                end_time = time_entry["endTime"]
-                timestamps.extend([start_time, end_time])
-
-print(timestamps)
 #print precipitation_probability plot
 x_precipitation_probability = timestamps
-y_precipitation_probability = precipitation_probability
+y_precipitation_probability = pop12h_values
 
 formatted_x = [f'{start} ->\n{end}' for start, end in zip(x_precipitation_probability[::2], x_precipitation_probability[1::2])]
 # Create a plot with labels and title
@@ -141,9 +85,29 @@ plt.show()
 
 #average temp plot
 y_temp = average_temp
-
+print(average_temp)
 # Create a plot with labels and title
 plt.plot(formatted_x, y_temp, label='average temperature')
+plt.xlabel('Time Range')
+plt.ylabel('Temperature')
+plt.title('Tamsui Weather Forecast')
+
+# Rotate x-axis labels
+plt.xticks(rotation=45)
+
+# Add grid lines and legend
+plt.grid(True)
+plt.legend()
+
+# Show the plot
+plt.tight_layout()
+plt.show()
+
+#MaxAT plot
+y_MaxAT = MaxAT
+print(average_temp)
+# Create a plot with labels and title
+plt.plot(formatted_x, y_MaxAT, label='MaxAT')
 plt.xlabel('Time Range')
 plt.ylabel('Temperature')
 plt.title('Tamsui Weather Forecast')
